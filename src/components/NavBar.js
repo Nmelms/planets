5c7d048318a8c6e2e14cf0af218c78d026cd6fd0
Nick Melms <nmelms92@gmail.com>
2022-08-18 00:03:35 -0400

    remade navbar for desktop

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import {Navbar, Row, Col, Container, Nav} from 'react-bootstrap'
-import hamburgerIcon from '../assets/icon-hamburger.svg'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Navbar, Row, Col, Container, Nav } from "react-bootstrap";
+import hamburgerIcon from "../assets/icon-hamburger.svg";
+import { Link } from "react-router-dom";
 
-
-
-export default function NavBar({handleClick}) {
-
- 
+export default function NavBar({ handleClick }) {
   return (
-    <div  >
-      <Navbar  variant="dark" className="p-3 navBar h-25  border-dark border-bottom"  >
+    <nav className="navBar">
+      {/* <Navbar  variant="dark" className="p-3 navBar h-25  border-dark border-bottom"  >
         <Row className=" d-flex navRow justify-content-center" className="w-100">
          <Col className="d-flex navCol" >
           <Navbar.Brand className="align-self-start navTitle ml-2" href="/">THE PLANETS</Navbar.Brand>
@@ -27,7 +23,38 @@ export default function NavBar({handleClick}) {
             </Nav>
             </Col>
         </Row>
-      </Navbar>
-    </div>
-  )
+      </Navbar> */}
+      <h2>
+        <Link className="navLink" to="/">
+          THE PLANETS
+        </Link>
+      </h2>
+      <div className="planetLinks">
+        <Link className="navLink" to="/mercury">
+          MERCURY
+        </Link>
+        <Link className="navLink" to="/venus">
+          VENUS
+        </Link>
+        <Link className="navLink" to="/">
+          EARTH
+        </Link>
+        <Link className="navLink" to="/mars">
+          MARS
+        </Link>
+        <Link className="navLink" to="/jupiter">
+          JUPITER
+        </Link>
+        <Link className="navLink" to="/saturn">
+          SATURN
+        </Link>
+        <Link className="navLink" to="/uranus">
+          URANUS
+        </Link>
+        <Link className="navLink" to="/neptune">
+          NEPTUNE
+        </Link>
+      </div>
+    </nav>
+  );
 }
